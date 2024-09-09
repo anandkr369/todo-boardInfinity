27b392be282252dc69bc3a3d8dce5eb2b4a194c2
ujjwal <ujjwal.12100544@lpu.in>
2024-09-09 22:36:55 +0530

    environment variables added

--- src/firebase.js
+++ src/firebase.js
@@ -8,13 +8,13 @@ import { getFirestore } from "firebase/firestore";
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: "AIzaSyCLp9MDRo9pP_zpSVFUhrbTjHBB74sIiug",
-  authDomain: "todo-5fbbc.firebaseapp.com",
-  projectId: "todo-5fbbc",
-  storageBucket: "todo-5fbbc.appspot.com",
-  messagingSenderId: "198367107920",
-  appId: "1:198367107920:web:7730dcb1e3c529cb2eaa55",
-  measurementId: "G-14CWWCFN63",
+  apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
+  authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.REACT_APP_FIREBASE_APP_ID,
+  measurementId: process.env.REACT_APP_FIREBASE_MEASUREMENT_ID,
 };
 
 // Initialize Firebase
